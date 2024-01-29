f8bd0243fafaa6c9951eccd28f9178ea72a5275f
Aaron Enyetu <aaronenyetu@gmail.com>
2024-01-29 12:04:35 -0800

    Created income expense and transaction list components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Header from './components/Header'
+import Balance from './components/Balance'
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
- <Header/>
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList/>
+      </div>
     </div>
   );
 }
