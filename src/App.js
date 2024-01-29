6bf803a6cdc28cc046c6ad9f4c245ff07b257432
Aaron Enyetu <aaronenyetu@gmail.com>
2024-01-29 12:14:05 -0800

    Added Add Transaction component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header'
 import Balance from './components/Balance'
 import IncomeExpenses from './components/IncomeExpenses';
 import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
 import './App.css';
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
       <div className='container'>
         <Balance />
         <IncomeExpenses />
-        <TransactionList/>
+        <TransactionList />
+        <AddTransaction/>
       </div>
     </div>
   );
